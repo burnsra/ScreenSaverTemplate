d6d90d6b78f139e0f70ec15119f0a6116b1680cd
Robert Burns <burnsra@gmail.com>
2017-11-28 19:45:19 -0500

    Initial Commit

---
+++ ScreenSaverTemplate/ScreenSaverTemplateView.h
@@ -0,0 +1,13 @@
+//
+//  ScreenSaverTemplateView.h
+//  ScreenSaverTemplate
+//
+//  Created by Robert Burns on 11/28/17.
+//  Copyright © 2017 Robert Burns. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ScreenSaverTemplateView : ScreenSaverView
+
+@end
---
+++ ScreenSaverTemplate/ScreenSaverTemplateView.m
@@ -0,0 +1,52 @@
+//
+//  ScreenSaverTemplateView.m
+//  ScreenSaverTemplate
+//
+//  Created by Robert Burns on 11/28/17.
+//  Copyright © 2017 Robert Burns. All rights reserved.
+//
+
+#import "ScreenSaverTemplateView.h"
+
+@implementation ScreenSaverTemplateView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
